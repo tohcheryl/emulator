73fbd9995124df4715c123fce119e11c49f61034
Cheryl Toh <e0035693@u.nus.edu>
2018-12-03 23:11:27 +0100

    Send and receive data via mqtt

--- src/main/java/Emulator.java
+++ src/main/java/Emulator.java
@@ -1,12 +1,10 @@
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class Emulator {
 
     public static void main(String[] args) {
+        receiveData();
         setData(14);
     }
 
@@ -15,7 +13,7 @@ public class Emulator {
         String content = "Sensor2,building=\"101\"" + " Temperature=" + inputData + ",batterylvl=12";
         int qos = 2;
         String broker = "tcp://se2-webapp04.compute.dtu.dk:1883";
-        String clientId = "339";
+        String clientId = "";
         MemoryPersistence persistence = new MemoryPersistence();
 
         try {
@@ -33,6 +31,40 @@ public class Emulator {
             sampleClient.disconnect();
             System.out.println("Disconnected");
             System.exit(0);
+        } catch (MqttException me) {
+            System.out.println("reason " + me.getReasonCode());
+            System.out.println("msg " + me.getMessage());
+            System.out.println("loc " + me.getLocalizedMessage());
+            System.out.println("cause " + me.getCause());
+            System.out.println("excep " + me);
+            me.printStackTrace();
+        }
+    }
+
+    public static void receiveData() {
+        String topic = "TempData";
+        int qos = 2;
+        String broker = "tcp://se2-webapp04.compute.dtu.dk:1883";
+        String clientId = "";
+        MemoryPersistence persistence = new MemoryPersistence();
+        try {
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            sampleClient.setCallback(new MqttCallback() {
+                public void connectionLost(Throwable cause) {}
+
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    System.out.println("Message received: " + message.toString());
+                }
+                public void deliveryComplete(IMqttDeliveryToken token) {}
+            });
+
+            System.out.println("Connecting to broker: " + broker);
+            sampleClient.connect(connOpts);
+            sampleClient.subscribe(topic);
+
+
         } catch (MqttException me) {
             System.out.println("reason " + me.getReasonCode());
             System.out.println("msg " + me.getMessage());
