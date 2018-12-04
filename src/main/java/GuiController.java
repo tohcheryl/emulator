452e08627e33ab13b60104918ba3a9d2d13ddb00
Cheryl Toh <e0035693@u.nus.edu>
2018-12-04 12:18:29 +0100

    Update UI to include Carbon Dioxide and Window Status

--- src/main/java/GuiController.java
+++ src/main/java/GuiController.java
@@ -12,22 +12,48 @@ public class GuiController {
     private TextField tempField;
 
     @FXML
-    private Label CurrentTemp;
+    private TextField carbonDioxideField;
+
+    @FXML
+    private TextField windowField;
+
+    @FXML
+    private Label currentTemp;
+
+    @FXML
+    private Label currentCarbonDioxide;
+
+    @FXML
+    private Label currentWindow;
 
     @FXML
     private Button setTempButton;
 
     @FXML
-    protected void handleSetTempButtonAction(ActionEvent event) {
+    protected void handleSetButtonAction(ActionEvent event) {
         int tempToSet = Integer.parseInt(tempField.getText());
-        handleCurrentTemp(tempToSet);
+        int carbonDioxideToSet = Integer.parseInt(carbonDioxideField.getText());
+        String windowStatusToSet = windowField.getText();
+
+        setTemp(tempToSet);
+        setCarbonDioxide(carbonDioxideToSet);
+        setWindow(windowStatusToSet);
+
         Emulator.setData(tempToSet);
         return;
     }
-    public void handleCurrentTemp(int temp){
-        CurrentTemp.setText((Integer.toString(temp)));
+
+    private void setTemp(int temp) {
+        currentTemp.setText((Integer.toString(temp)));
     }
 
+    private void setCarbonDioxide(int carbonDioxideLevel) {
+        currentCarbonDioxide.setText(Integer.toString(carbonDioxideLevel));
+    }
+
+    private void setWindow(String status) {
+        currentWindow.setText(status);
+    }
 
 
 }
